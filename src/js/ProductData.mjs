24e2971af4f0054ab9d90b0b55ade21a6aaca29b
Yeampier2022 <yeampierjesusa@gmail.com>
2023-09-25 09:34:11 -0400

    feat: week 1 and 2

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -2,7 +2,7 @@ function convertToJson(res) {
   if (res.ok) {
     return res.json();
   } else {
-    throw new Error("Bad Response");
+    throw new Error(`Bad Response`);
   }
 }
 
@@ -11,11 +11,14 @@ export default class ProductData {
     this.category = category;
     this.path = `../json/${this.category}.json`;
   }
-  getData() {
-    return fetch(this.path)
-      .then(convertToJson)
-      .then((data) => data);
+
+  async getData() {
+    return (await convertToJson(await fetch(this.path))).map((item) => ({
+      ...item,
+      category: this.category,
+    }));
   }
+
   async findProductById(id) {
     const products = await this.getData();
     return products.find((item) => item.Id === id);
